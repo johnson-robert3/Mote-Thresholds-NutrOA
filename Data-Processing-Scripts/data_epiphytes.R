b6b0e0c0fc95f6aa5369088fb151a1ac7dcab9c9
Robert Johnson <johnson.robert.333@gmail.com>
2026-05-18 17:45:45 -0500

    update after removing some of the ind. shoot vars from the morph df

--- Data-Processing-Scripts/data_epiphytes.R
+++ Data-Processing-Scripts/data_epiphytes.R
@@ -22,7 +22,7 @@ tmp1 <- tmp %>%
    # shoot biomass
    left_join(biomass_plant %>% select(plant_id, week, species, shoot_biomass_g)) %>%
    # morphometry
-   left_join(morph_plant %>% select(plant_id, week, species, starts_with("num_"), starts_with("blade_"), tot_leaf_area)) 
+   left_join(morph_plant %>% select(plant_id, week, species, blade_length, blade_width, tot_leaf_area)) 
 
 
 # standardize epiphytes to other vars
@@ -30,12 +30,8 @@ epi_plant <- tmp1 %>%
    mutate(
       # to blade length (BL) (units = mg/cm)
       epi_by_BL = epi_biomass_g*1000 / blade_length,
-      # to individual blade area (BA) (units = mg/cm^2)
-      epi_by_BA = epi_biomass_g*1000 / blade_area,
       # to total leaf surface area (leafSA) (units = mg/cm^2)
-      epi_by_leafSA = epi_biomass_g*1000 / tot_leaf_area,
-      # to number of blades (units = mg/blade)
-      epi_by_blades = epi_biomass_g*1000 / num_blades)
+      epi_by_leafSA = epi_biomass_g*1000 / tot_leaf_area)
 
 
 
