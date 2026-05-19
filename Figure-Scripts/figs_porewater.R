10a9116ba4f095fb9c98d03c3665a78260625adc
Robert Johnson <johnson.robert.333@gmail.com>
2026-05-19 15:51:50 -0500

    add 3-panel figs across periods

--- Figure-Scripts/figs_porewater.R
+++ Figure-Scripts/figs_porewater.R
@@ -64,6 +64,30 @@ ggplot(porewater_plant %>% filter(week == "w9" & treatment_nutrients!="pulsed") 
 ggsave("C:/Users/rajohnson6/Desktop/Local-Repos/Seagrass-NutrOA-Thresholds/pw_pH_w9.png", height=4, width=5, units="in", dpi=300)
 
 
+# ph - acclimation, stress, recovery periods
+ggplot(porewater_plant %>% filter(week %in% c('w2', 'w6', 'w9') & treatment_nutrients!="pulsed") %>%
+          mutate(treatment_nutrients = parse_number(treatment_nutrients) %>% as.factor()) %>%
+          summarize(mean = mean(pH, na.rm=TRUE),
+                    se = se(pH),
+                    .by = c(treatment_ph, treatment_nutrients, week))) +
+   #
+   geom_line(aes(x = treatment_nutrients, y = mean, color = treatment_ph, group = treatment_ph), 
+             position = position_dodge(width=0.3), linewidth=0.75, alpha=0.4) +
+   geom_errorbar(aes(x = treatment_nutrients, y = mean, ymin = mean - se, ymax = mean + se, color = treatment_ph), 
+                 position = position_dodge(width=0.3), width=0, linewidth=0.67) +
+   geom_point(aes(x = treatment_nutrients, y = mean, color = treatment_ph), 
+              size=3.5, shape=19, position = position_dodge(width=0.3)) +
+   scale_color_manual(name = 'pH', values = ph_col_env) +
+   labs(title = "Porewater pH",
+        x = "Nutrient treatment (g)",
+        y = "pH") +
+   facet_wrap(facets = vars(week)) +
+   theme_classic() %>%
+   fig_theme()
+
+ggsave("C:/Users/rajohnson6/Desktop/Local-Repos/Seagrass-NutrOA-Thresholds/pw_pH_periods.png", height=3, width=8, units="in", dpi=300)
+
+
 
 #--
 # Sulfide
@@ -103,6 +127,29 @@ ggplot(porewater_trt %>% filter(week == "w9" & treatment_nutrients!="pulsed") %>
         x = "Nutrient treatment",
         y = "Total dissolved sulfide (uM)")
 
+
+# DS - acclimation, stress, recovery periods
+ggplot(porewater_plant %>% filter(week %in% c('w2', 'w6', 'w9') & treatment_nutrients!="pulsed") %>%
+          mutate(treatment_nutrients = parse_number(treatment_nutrients) %>% as.factor()) %>%
+          summarize(mean = mean(porewater_S_uM, na.rm=TRUE),
+                    se = se(porewater_S_uM),
+                    .by = c(treatment_ph, treatment_nutrients, week))) +
+   #
+   geom_line(aes(x = treatment_nutrients, y = mean, color = treatment_ph, group = treatment_ph), 
+             position = position_dodge(width=0.3), linewidth=0.75, alpha=0.4) +
+   geom_errorbar(aes(x = treatment_nutrients, y = mean, ymin = mean - se, ymax = mean + se, color = treatment_ph), 
+                 position = position_dodge(width=0.3), width=0, linewidth=0.67) +
+   geom_point(aes(x = treatment_nutrients, y = mean, color = treatment_ph), 
+              size=3.5, shape=19, position = position_dodge(width=0.3)) +
+   scale_color_manual(name = 'pH', values = ph_col_env) +
+   labs(title = "Porewater sulfide",
+        x = "Nutrient treatment (g)",
+        y = "Total dissolved sulfide (uM)") +
+   facet_wrap(facets = vars(week)) +
+   theme_classic() %>%
+   fig_theme()
+
+ggsave("C:/Users/rajohnson6/Desktop/Local-Repos/Seagrass-NutrOA-Thresholds/pw_sulfide_periods.png", height=3, width=8, units="in", dpi=300)
 
 
 
