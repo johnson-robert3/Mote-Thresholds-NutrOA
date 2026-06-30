a13a17fb88e70135a041da32497da3caf250dfa5
Robert Johnson <johnson.robert.333@gmail.com>
2026-06-29 22:22:45 -0500

    add new nesting method to calc for multiple variables at once

--- Analysis-Scripts/test_effect-sizes.R
+++ Analysis-Scripts/test_effect-sizes.R
@@ -7,18 +7,14 @@
 
 # set up data
 
-tmp <- shoots_plant %>%
-   # remove wks 3 and 8 (only dead/missing plants recorded these weeks; number of blades/shoots were not counted if plant was present)
-   filter_out(week %in% c('w3', 'w8')) %>%
-   # replace 0's with NA, so that missing plants are excluded from mean values
-   mutate(across(c(shoot_count, leaf_count, bps), ~replace_values(., 0 ~ NA_real_))) %>% 
-   # treatment means
-   summarize(across(c(shoot_count, leaf_count, bps), list(mean=~mean(., na.rm=TRUE), se=se, sd=~sd(., na.rm=TRUE)), .names="{.fn}_{.col}"), 
-             n=n(),
-             .by=c(treatment_ph, treatment_nutrients, week, species)) %>%
+#' I think I need to do these one variable at a time (may be able to to all together, but df will get very wide...)
+
+# test with BPS
+tmp <- shoots_trt %>%
+   select(treatment_ph, treatment_nutrients, week, species, contains('bps'), n) %>%
    #
-   filter(week == "w9", species == "Hw") %>%
-   # group_by(treatment_nutrients) %>%
+   # filter(week == "w9", species == "Hw") %>%
+   group_by(species, week, treatment_nutrients) %>%
    summarize(
       # mean difference
       mean_diff = mean_bps[treatment_ph == "OA"] - mean_bps[treatment_ph == "ambient"],
@@ -27,7 +23,54 @@ tmp <- shoots_plant %>%
       # log ratio
       log_ratio = log(mean_bps[treatment_ph == "OA"]) - log(mean_bps[treatment_ph == "ambient"]),
       # groups
-      .by = c(species, week, treatment_nutrients))
+      .groups = 'drop')
+
+
+
+# alternative method to calculate effect size stats on multiple variables at once
+
+#' lengthen the df so there is a 'parameter' and a 'result' column
+#' then nest by 'parameter' column, and compute summary stats and effect sizes for each of the parameters
+
+tmp <- shoots_plant %>%
+   select(plant_id, treatment_ph, treatment_nutrients, week, species, shoot_count, leaf_count, bps) %>%
+   filter_out(week %in% c('w3', 'w8')) %>%
+   pivot_longer(cols = where(is.numeric), 
+                names_to = 'parameter',
+                values_to = 'result') %>%
+   nest(.by = parameter) %>%
+   mutate(summary_stats = map(data, 
+                              ~{.x %>%
+                                    group_by(species, treatment_ph, treatment_nutrients, week) %>% 
+                                    summarize(mean = mean(result, na.rm=TRUE),
+                                              sd = sd(result, na.rm=TRUE),
+                                              se = se(result),
+                                              n = n(),
+                                              .groups = 'drop')}),
+          effect_sizes = map(summary_stats, 
+                             ~{.x %>% 
+                                   group_by(species, week, treatment_nutrients) %>%
+                                   summarize(
+                                      # mean difference
+                                      mean_diff = mean[treatment_ph == "OA"] - mean[treatment_ph == "ambient"],
+                                      # Hedges' g (mean_diff / SD_pooled)
+                                      hedges_g = mean_diff / sqrt(((sd[treatment_ph == "OA"])^2 + (sd[treatment_ph == "ambient"])^2) / 2),
+                                      # log response ratio
+                                      log_ratio = log(mean[treatment_ph == "OA"]) - log(mean[treatment_ph == "ambient"]),
+                                      .groups = 'drop')})
+   )
+
+
+# df of effect sizes
+shoots_effect_sizes <- tmp %>%
+   select(parameter, effect_sizes) %>%
+   unnest(effect_sizes)
+
+# df of summary stats
+shoots_trt_means <- tmp %>%
+   select(parameter, summary_stats) %>%
+   unnest(summary_stats)
+
 
 
 
