4719d4076357eda833e61527c0acefd027981011
Robert Johnson <johnson.robert.333@gmail.com>
2026-06-30 23:24:29 -0500

    remove other method, using a nest() and map() structure, that was no longer needed

--- Analysis-Scripts/test_effect-sizes.R
+++ Analysis-Scripts/test_effect-sizes.R
@@ -8,54 +8,6 @@
 # set up data
 
 
-# alternative method to calculate effect size stats on multiple variables at once
-
-#' lengthen the df so there is a 'parameter' and a 'result' column
-#' then nest by 'parameter' column, and compute summary stats and effect sizes for each of the parameters
-
-tmp <- shoots_plant %>%
-   select(plant_id, treatment_ph, treatment_nutrients, week, species, shoot_count, leaf_count, bps) %>%
-   # not correct to just fully omit w3
-   filter_out(week %in% c('w3', 'w8')) %>%
-   pivot_longer(cols = where(is.numeric), 
-                names_to = 'parameter',
-                values_to = 'result') %>%
-   nest(.by = parameter) %>%
-   # have not yet removed dead/missing plants; shouldn't be included in summary stats
-   mutate(summary_stats = map(data, 
-                              ~{.x %>%
-                                    group_by(species, treatment_ph, treatment_nutrients, week) %>% 
-                                    summarize(mean = mean(result, na.rm=TRUE),
-                                              sd = sd(result, na.rm=TRUE),
-                                              se = se(result),
-                                              n = n(),
-                                              .groups = 'drop')}),
-          effect_sizes = map(summary_stats, 
-                             ~{.x %>% 
-                                   group_by(species, week, treatment_nutrients) %>%
-                                   summarize(
-                                      # mean difference
-                                      mean_diff = mean[treatment_ph == "OA"] - mean[treatment_ph == "ambient"],
-                                      # Hedges' g (mean_diff / SD_pooled)
-                                      hedges_g = mean_diff / sqrt(((sd[treatment_ph == "OA"])^2 + (sd[treatment_ph == "ambient"])^2) / 2),
-                                      # log response ratio
-                                      log_ratio = log(mean[treatment_ph == "OA"]) - log(mean[treatment_ph == "ambient"]),
-                                      .groups = 'drop')})
-   )
-
-
-# df of effect sizes
-shoots_effect_sizes <- tmp %>%
-   select(parameter, effect_sizes) %>%
-   unnest(effect_sizes)
-
-# df of summary stats
-shoots_trt_means <- tmp %>%
-   select(parameter, summary_stats) %>%
-   unnest(summary_stats)
-
-
-
 #' another alternative method; no need to use nest() and map(), can just lengthen and then compute on the df, grouping by the parameter variable
 #' need to make dfs for summary stats and for effect sizes separately this way, though
 
